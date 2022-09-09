dd09a54efcc72893224a21dad089fd235c830a3e
KHodow677 <kylehodowany@utexas.edu>
2022-09-09 11:49:46 -0500

    Finished Backtracking Solver Logic

--- Sodoku.py
+++ Sodoku.py
@@ -3,3 +3,6 @@ import Board, SodokuSolver
 problem = "780400120600075009000601078007040260001050930904060005070300012120007400049206007"
 mySodokuSolver = SodokuSolver.SodokuSolver(problem)
 mySodokuSolver.PrintBoard(mySodokuSolver.board.board)
+print("___________________________________________________________")
+mySodokuSolver.Solve(mySodokuSolver.board.board)
+mySodokuSolver.PrintBoard(mySodokuSolver.board.board)
